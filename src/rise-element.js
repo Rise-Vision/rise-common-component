ff68cbfbed79d9e27cd27b186ec6264fd951a9da
Alex Deaconu <alex.deaconu@gmail.com>
2019-06-26 09:58:48 -0400

    Fix eslint

--- src/rise-element.js
+++ src/rise-element.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-console */
-
 import { PolymerElement } from "@polymer/polymer";
 import { dedupingMixin } from "@polymer/polymer/lib/utils/mixin.js";
 
@@ -9,103 +7,102 @@ import { version } from "./rise-element-version.js";
 
 export const RiseElementMixin = dedupingMixin( base => {
 
-  const riseElementBase = LoggerMixin( base );
+    const riseElementBase = LoggerMixin( base );
 
-  class riseElement extends riseElementBase {
+    class riseElement extends riseElementBase {
 
-    static get properties() {
-      return {
+      static get properties() {
+        return {
         /**
          * Component version; should override when implementing
          */
-        version: {
-          type: String,
-          value: version,
-          readOnly: true
+          version: {
+            type: String,
+            value: version,
+            readOnly: true
+          }
         }
       }
-    }
 
-    // Event name constants
-    static get EVENT_CONFIGURED() {
-      return "configured";
-    }
-    static get EVENT_START() {
-      return "start";
-    }
-
-    constructor() {
-      super();
-
-      this._uptimeError = false;
-    }
-
-    connectedCallback() {
-      super.connectedCallback();
-      this._uptimeHandler = this._handleUptimeRequest.bind( this );
-      window.addEventListener( "component-uptime-request", this._uptimeHandler );
-    }
-
-    disconnectedCallback() {
-      super.disconnectedCallback();
-      window.removeEventListener( "component-uptime-request", this._uptimeHandler );
-    }
-
-    ready() {
-      super.ready();
-
-      super.initLogger({
-        name: this.tagName.toLowerCase(),
-        id: this.id,
-        version: this.version
-      });
-
-      if ( RisePlayerConfiguration.isConfigured()) {
-        this._init();
-      } else {
-        const init = () => this._init();
-
-        window.addEventListener( "rise-components-ready", init, { once: true });
+      // Event name constants
+      static get EVENT_CONFIGURED() {
+        return "configured";
       }
-    }
+      static get EVENT_START() {
+        return "start";
+      }
 
-    _init() {
-      this.addEventListener( RiseElement.EVENT_START, this._handleStart, { once: true });
+      constructor() {
+        super();
 
-      this._sendEvent( RiseElement.EVENT_CONFIGURED );
-    }
+        this._uptimeError = false;
+      }
 
-    _sendEvent( eventName, detail = {}) {
-      const event = new CustomEvent( eventName, {
-        bubbles: true, composed: true, detail
-      });
+      connectedCallback() {
+        super.connectedCallback();
+        this._uptimeHandler = this._handleUptimeRequest.bind( this );
+        window.addEventListener( "component-uptime-request", this._uptimeHandler );
+      }
 
-      this.dispatchEvent( event );
-    }
+      disconnectedCallback() {
+        super.disconnectedCallback();
+        window.removeEventListener( "component-uptime-request", this._uptimeHandler );
+      }
 
-    _handleStart() {
-      super.log( "info", "start received" );
-    }
+      ready() {
+        super.ready();
 
-    _handleUptimeRequest() {
-      window.dispatchEvent( new CustomEvent( "component-uptime-result", {
-        detail: {
-          component_id: this.id,
-          component_type: this.tagName.toLowerCase(),
-          error: this._uptimeError
+        super.initLogger({
+          name: this.tagName.toLowerCase(),
+          id: this.id,
+          version: this.version
+        });
+
+        if ( RisePlayerConfiguration.isConfigured()) {
+          this._init();
+        } else {
+          const init = () => this._init();
+
+          window.addEventListener( "rise-components-ready", init, { once: true });
         }
-      }));
+      }
+
+      _init() {
+        this.addEventListener( RiseElement.EVENT_START, this._handleStart, { once: true });
+
+        this._sendEvent( RiseElement.EVENT_CONFIGURED );
+      }
+
+      _sendEvent( eventName, detail = {}) {
+        const event = new CustomEvent( eventName, {
+          bubbles: true, composed: true, detail
+        });
+
+        this.dispatchEvent( event );
+      }
+
+      _handleStart() {
+        super.log( "info", "start received" );
+      }
+
+      _handleUptimeRequest() {
+        window.dispatchEvent( new CustomEvent( "component-uptime-result", {
+          detail: {
+            component_id: this.id,
+            component_type: this.tagName.toLowerCase(),
+            error: this._uptimeError
+          }
+        }));
+      }
+
+      _setUptimeError( value ) {
+        this._uptimeError = value;
+      }
+
     }
 
-    _setUptimeError( value ) {
-      this._uptimeError = value;
-    }
-
-  }
-
-  return riseElement;
-});
-
-export const RiseElement = RiseElementMixin( PolymerElement );
+    return riseElement;
+  }),
+  RiseElement = RiseElementMixin( PolymerElement );
 
 customElements.define( "rise-element", RiseElement );
