81e69bee45ba352c236abb78e49075d52fec3734
William Blanchette <will@Williams-MBP.hitronhub.home>
2019-07-12 07:57:12 -0600

    Stub out basic ValidFilesMixin
    Add a basic mixin for validating files and associated tests

---
+++ src/valid-files-mixin.js
@@ -0,0 +1,79 @@
+import { dedupingMixin } from "@polymer/polymer/lib/utils/mixin.js";
+
+export const ValidFilesMixin = dedupingMixin( base => {
+  const VALID_FILES_CONFIG = {
+    name: "valid-files-mixin",
+    id: "valid-files",
+    version: "1.0"
+  };
+
+  class ValidFiles extends base {
+    constructor() {
+      super();
+
+      this.validFilesConfig = Object.assign({}, VALID_FILES_CONFIG );
+    }
+
+    initValidFiles( validFilesConfig ) {
+      Object.assign( this.validFilesConfig, validFilesConfig );
+    }
+
+    getStorageFileFormat( filePath ) {
+      if ( !filePath || typeof filePath !== "string" ) {
+        return "";
+      }
+
+      return filePath.substr( filePath.lastIndexOf( "." ) + 1 ).toLowerCase();
+    }
+
+    isValidFileType( path, validTypes ) {
+      const format = this.getStorageFileFormat( path );
+
+      for ( let i = 0, len = validTypes.length; i < len; i++ ) {
+        if ( format.indexOf( validTypes[ i ]) !== -1 ) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    isValidFiles( files ) {
+      if ( !files || typeof files !== "string" ) {
+        return false;
+      }
+
+      // single symbol
+      if ( files.indexOf( "|" ) === -1 ) {
+        return true;
+      }
+
+      return files.split( "|" ).indexOf( "" ) === -1;
+    }
+
+    filterInvalidFileTypes( files ) {
+      let invalidFiles = [];
+      const validFiles = files.filter( file => {
+        const valid = this.isValidFileType( file );
+
+        if ( !valid ) {
+          invalidFiles.push( file );
+        }
+
+        return valid;
+      });
+
+      return { validFiles, invalidFiles };
+    }
+
+    validateFiles( files ) {
+      if ( !this.isValidFiles( files )) {
+        return { validFiles: [], invalidFiles: [] };
+      }
+
+      return this.filterInvalidFileTypes( files.split( "|" ));
+    }
+  }
+
+  return ValidFiles;
+})
