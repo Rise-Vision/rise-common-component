8e18e5b70eab0ccc50ffcebd6fbd0fcb9ad2663c
Alex Deaconu <alex.deaconu@gmail.com>
2019-09-10 12:52:39 -0400

    Implement cache API fallback via localStorage

--- src/cache-mixin.js
+++ src/cache-mixin.js
@@ -9,11 +9,94 @@ export const CacheMixin = dedupingMixin( base => {
     },
     cacheBase = LoggerMixin( base );
 
+  class LocalStorageFallback {
+    open( namespace ) {
+      const getFullKey = key => {
+          return namespace + key;
+        },
+        tryParse = text => {
+          try {
+            return JSON.parse( text );
+          } catch ( e ) {
+            return {};
+          }
+        },
+        cacheToResponse = response => {
+          const cacheObject = {
+            headers: [],
+            status: response.status,
+            statusText: response.statusText
+          };
+
+          for ( let p of response.headers.entries()) {
+            cacheObject.headers.push( p );
+          }
+
+          return response.clone().text().then( text => {
+            cacheObject.text = text;
+
+            return JSON.stringify( cacheObject );
+          });
+        },
+        responseToCache = text => {
+          const cacheObject = tryParse( text ),
+            init = {
+              status: cacheObject.status,
+              statusText: cacheObject.statusText,
+              headers: cacheObject.headers
+            },
+            response = new Response( new Blob([ cacheObject.text ]), init );
+
+          return response;
+        },
+        cacheMock = {
+          keys: () => {
+            const keys = [];
+
+            for ( let i = 0, len = localStorage.length; i < len; ++i ) {
+              if ( localStorage.key( i ).startsWith( namespace )) {
+                const key = localStorage.key( i ).slice( namespace.length );
+
+                keys.push( key );
+              }
+            }
+
+            return Promise.resolve( keys );
+          },
+          match: ( key ) => {
+            const item = localStorage.getItem( getFullKey( key ));
+
+            if ( item ) {
+              let response = responseToCache( item );
+
+              return Promise.resolve( response );
+            } else {
+              return Promise.reject();
+            }
+          },
+          delete: ( key ) => {
+            localStorage.removeItem( getFullKey( key ));
+          },
+          put: ( key, value ) => {
+            cacheToResponse( value ).then( text => {
+              localStorage.setItem( getFullKey( key ), text );
+            });
+          }
+        };
+
+      return Promise.resolve( cacheMock );
+    }
+  }
+
   class Cache extends cacheBase {
     constructor() {
       super();
 
       this.cacheConfig = Object.assign({}, CACHE_CONFIG );
+
+      if ( !( window.caches && window.caches.open )) {
+        window.caches = new LocalStorageFallback();
+      }
     }
 
     initCache( cacheConfig ) {
@@ -26,7 +109,7 @@ export const CacheMixin = dedupingMixin( base => {
       if ( window.caches && window.caches.open ) {
         return caches.open( this.cacheConfig.name );
       } else {
-        super.log( "warning", "cache API not available" );
+        super.log( "warning", "cache API not available", true );
 
         return Promise.reject();
       }
@@ -53,7 +136,7 @@ export const CacheMixin = dedupingMixin( base => {
             });
           });
         });
-      });
+      }).catch(() => {});
     }
 
     putCache( res, url ) {
@@ -65,7 +148,7 @@ export const CacheMixin = dedupingMixin( base => {
     }
 
     getCache( url ) {
-      var _cache;
+      let _cache;
 
       return this._getCache().then( cache => {
         _cache = cache;
@@ -75,9 +158,11 @@ export const CacheMixin = dedupingMixin( base => {
           return Promise.resolve( response );
         } else if ( !this._isResponseExpired( response, this.cacheConfig.expiry )) {
           return Promise.reject( response );
-        } else {
+        } else if ( _cache ) {
           response && _cache.delete( url );
 
+          return Promise.reject();
+        } else {
           return Promise.reject();
         }
       });
