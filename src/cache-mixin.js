8ba02ff7c389708945320146c5b9c8eedbddbefa
Alex Deaconu <alex.deaconu@gmail.com>
2019-09-16 12:05:38 -0400

    Implement cache API fallback via localStorage (#27)
    Log cache and localStorage availability
    Improved error catching
    Add url to response object
    
    Log client offline as warning and only once
    Player offline state is not an error state
    and should be handled correctly by caching

--- src/cache-mixin.js
+++ src/cache-mixin.js
@@ -9,11 +9,101 @@ export const CacheMixin = dedupingMixin( base => {
     },
     cacheBase = LoggerMixin( base );
 
+  class LocalStorageFallback {
+    open( namespace ) {
+      const getFullKey = key => {
+          return namespace + key;
+        },
+        tryParse = text => {
+          try {
+            return JSON.parse( text );
+          } catch ( e ) {
+            return {};
+          }
+        },
+        responseToCache = response => {
+          const cacheObject = {
+            headers: [],
+            status: response.status,
+            statusText: response.statusText,
+            url: response.url
+          };
+
+          response.headers.forEach(( val, key ) => {
+            cacheObject.headers.push([ key, val ]);
+          })
+
+          return response.clone().text().then( text => {
+            cacheObject.text = text;
+
+            return JSON.stringify( cacheObject );
+          });
+        },
+        cacheToResponse = text => {
+          const cacheObject = tryParse( text ),
+            init = {
+              status: cacheObject.status,
+              statusText: cacheObject.statusText,
+              headers: cacheObject.headers
+            },
+            response = new Response( new Blob([ cacheObject.text ]), init );
+
+          if ( cacheObject.url ) {
+            Object.defineProperty( response, "url", { value: cacheObject.url });
+          }
+
+          return response;
+        },
+        cacheMock = {
+          keys: () => {
+            const keys = [];
+
+            for ( let i = 0, len = localStorage.length; i < len; ++i ) {
+              if ( localStorage.key( i ).startsWith( namespace )) {
+                const key = localStorage.key( i ).slice( namespace.length );
+
+                keys.push( key );
+              }
+            }
+
+            return Promise.resolve( keys );
+          },
+          match: ( key ) => {
+            const item = localStorage.getItem( getFullKey( key ));
+
+            if ( item ) {
+              let response = cacheToResponse( item );
+
+              return Promise.resolve( response );
+            } else {
+              return Promise.resolve();
+            }
+          },
+          delete: ( key ) => {
+            localStorage.removeItem( getFullKey( key ));
+          },
+          put: ( key, value ) => {
+            responseToCache( value ).then( text => {
+              localStorage.setItem( getFullKey( key ), text );
+            });
+          }
+        };
+
+      return Promise.resolve( cacheMock );
+    }
+  }
+
   class Cache extends cacheBase {
     constructor() {
       super();
 
       this.cacheConfig = Object.assign({}, CACHE_CONFIG );
+
+      if ( window.caches && window.caches.open ) {
+        this._caches = window.caches;
+      } else if ( window.localStorage ) {
+        this._caches = new LocalStorageFallback();
+      }
     }
 
     initCache( cacheConfig ) {
@@ -23,10 +113,14 @@ export const CacheMixin = dedupingMixin( base => {
     }
 
     _getCache() {
-      if ( window.caches && window.caches.open ) {
-        return caches.open( this.cacheConfig.name );
+      if ( this._caches ) {
+        if ( !( window.caches && window.caches.open )) {
+          super.log( "warning", "cache API not available", true );
+        }
+
+        return this._caches.open( this.cacheConfig.name );
       } else {
-        super.log( "warning", "cache API not available" );
+        super.log( "warning", "localStorage fallback API not available", true );
 
         return Promise.reject();
       }
@@ -43,44 +137,55 @@ export const CacheMixin = dedupingMixin( base => {
     }
 
     _deleteExpiredCache() {
-      this._getCache().then( cache => {
-        cache.keys().then( keys => {
-          keys.forEach( key => {
-            cache.match( key ).then( response => {
-              if ( this._isResponseExpired( response, this.cacheConfig.expiry )) {
-                cache.delete( key );
-              }
+      if ( this._caches ) {
+        this._getCache().then( cache => {
+          return cache.keys().then( keys => {
+            keys.forEach( key => {
+              cache.match( key ).then( response => {
+                if ( response && this._isResponseExpired( response, this.cacheConfig.expiry )) {
+                  cache.delete( key );
+                }
+              });
             });
           });
         });
-      });
+      }
     }
 
-    putCache( res, url ) {
-      return this._getCache().then( cache => {
-        return cache.put( res.url || url, res );
-      }).catch( err => {
-        super.log( "warning", "cache put failed", { url: res.url || url, err });
-      });
+    putCache( response, url ) {
+      if ( this._caches ) {
+        return this._getCache().then( cache => {
+          return cache.put( response.url || url, response );
+        }).catch( err => {
+          super.log( "warning", "cache put failed", { url: response.url || url, err });
+        });
+      } else {
+        return Promise.resolve();
+      }
     }
 
     getCache( url ) {
-      var _cache;
+      if ( this._caches ) {
+        let _cache;
 
-      return this._getCache().then( cache => {
-        _cache = cache;
-        return cache.match( url );
-      }).then( response => {
-        if ( !this._isResponseExpired( response, this.cacheConfig.refresh )) {
-          return Promise.resolve( response );
-        } else if ( !this._isResponseExpired( response, this.cacheConfig.expiry )) {
-          return Promise.reject( response );
-        } else {
-          response && _cache.delete( url );
-
-          return Promise.reject();
-        }
-      });
+        return this._getCache().then( cache => {
+          _cache = cache;
+          return cache.match( url );
+        }).then( response => {
+          if ( !this._isResponseExpired( response, this.cacheConfig.refresh )) {
+            return Promise.resolve( response );
+          } else if ( !this._isResponseExpired( response, this.cacheConfig.expiry )) {
+            return Promise.reject( response );
+          } else if ( _cache ) {
+            response && _cache.delete( url );
+            return Promise.reject();
+          } else {
+            return Promise.reject();
+          }
+        });
+      } else {
+        return Promise.reject();
+      }
     }
   }
 
