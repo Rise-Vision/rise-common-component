91e67ddd9d5df4490e989aba3060e8e237aa6ed8
Ezequiel Conte <ezequielc@gmail.com>
2020-05-08 23:35:23 -0300

    Update max-age to cache Shared Schedules requests

--- scripts/deploy-gcs.sh
+++ scripts/deploy-gcs.sh
@@ -2,5 +2,5 @@ COMPONENT_NAME=$1
 TARGET=$2
 
 gsutil cp dist/$COMPONENT_NAME.js $TARGET
-gsutil -m setmeta -r -h "Cache-Control:private, max-age=0" $TARGET
+gsutil -m setmeta -r -h "Cache-Control:private, max-age=600" $TARGET
 gsutil acl -r ch -u AllUsers:R $TARGET
