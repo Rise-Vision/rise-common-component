a34d4839bdcc8b98260322426b2b62386aed1ceb
Stepanov <daniil.stepanov@requestum.com>
2020-06-11 23:16:13 +0300

    work for today 06.11.2020: code for checking version of the file and handlind 200 and 304 response

--- src/store-files-mixin.js
+++ src/store-files-mixin.js
@@ -16,36 +16,85 @@ export const StoreFilesMixin = dedupingMixin( base => {
     }
 
     getFile( fileUrl ) {
-      return super.getCache( fileUrl ).then(() => {
-        return this._handleCachedFile();
+      return super.getCache( fileUrl ).then( cache => {
+        console.log('CACHED');
+        return this._handleCachedFile( fileUrl, cache );
       }).catch(() => {
+        console.log('REQUESTED');        
         return this._requestFile( fileUrl );
       })
     }
 
-    _handleCachedFile() {
-      // TODO: Check file version and update it if needed and return it.
+    _handleCachedFile( fileUrl, cache ) {
+      console.log( "cache", cache );
+      let dateSinceModified = 1561661041,
+        respToCache;
+
+      // return fetch( fileUrl )
+      return fetch( fileUrl, {
+          headers: {
+            "If-Modified-Since": dateSinceModified
+          },
+          mode: "cors" 
+        } )
+        .then( resp => {
+          if ( resp.status === 200 ) {
+            respToCache = resp.clone();
+            super.putCache( respToCache );
+            return this._getFileRepresentation( resp );
+          } else if ( resp.status === 304 ) {
+            return this._getFileRepresentation( cache );
+          }
+        })
+        .catch( err => {
+          console.error( "", err );
+        })
+      // TODO: send if-match-since request
+      // TODO:   check response.status
+      // TODO:     handle 200 status code
+      // TODO:       putCache
+      // TODO:       _getFileRepresentation
+      // TODO:     handle 300 status code
+      // TODO:       _getFileRepresentation
     }
 
     _requestFile( fileUrl ) {
-      let response;
+      let respToCache;
 
       return fetch( fileUrl )
         .then( resp => {
-          response = resp.clone();
-          return resp.blob();
-        })
-        .then( blob => {
-          return URL.createObjectURL( blob );
+          respToCache = resp.clone();
+          return this._getFileRepresentation( resp );
         })
         .then( objectURL => {
-          super.putCache( response );
+          super.putCache( respToCache );
           return objectURL;
         })
         .catch(() => {
           // TODO: handle errors
         })
     }
+
+    _getFileRepresentation( resp ) {
+      return resp.blob().then( blob => {
+        return URL.createObjectURL( blob );
+      })
+    }
+
+    _getCacheCustom() {
+      if ( this._caches ) {
+        let _cache;
+
+        return this._getCache().then( cache => {
+          _cache = cache;
+          return cache.match( this.getCacheRequestKey( url ));
+        })
+      } else {
+        return Promise.reject();
+      }
+    }
+
+  }
 
   }
 
