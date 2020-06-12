ec5a36e1464dcbad07c1d59b6042a36c06ded362
Stepanov <daniil.stepanov@requestum.com>
2020-06-12 14:25:57 +0300

    added saving last requested timestamp + code refactor + clearup

--- src/store-files-mixin.js
+++ src/store-files-mixin.js
@@ -67,36 +67,32 @@ export const StoreFilesMixin = dedupingMixin( base => {
     constructor() {
       super();
 
-      this.cacheConfig = Object.assign({}, CACHE_CONFIG );
       this.lastRequestedStorage = new LastRequestedStorage();
+      this.cacheConfig = Object.assign({}, CACHE_CONFIG );
     }
 
     getFile( fileUrl ) {
-      return super.getCache( fileUrl ).then( cache => {
-        console.log('CACHED');
+      return this._getCacheCustom( fileUrl ).then( cache => {
         return this._handleCachedFile( fileUrl, cache );
       }).catch(() => {
-        console.log('REQUESTED');        
         return this._requestFile( fileUrl );
       })
     }
 
     _handleCachedFile( fileUrl, cache ) {
-      console.log( "cache", cache );
-      let dateSinceModified = 1561661041,
-        respToCache;
+      let respToCache;
 
-      // return fetch( fileUrl )
-      return fetch( fileUrl, {
-          headers: {
-            "If-Modified-Since": dateSinceModified
-          },
-          mode: "cors" 
-        } )
+      return fetch( fileUrl )
+      // return fetch( fileUrl, {
+      //     headers: {
+      //       "If-Modified-Since": dateSinceModified
+      //     }
+      //   } )
         .then( resp => {
           if ( resp.status === 200 ) {
             respToCache = resp.clone();
             super.putCache( respToCache );
+            this.lastRequestedStorage.save( fileUrl, Date.now());
             return this._getFileRepresentation( resp );
           } else if ( resp.status === 304 ) {
             return this._getFileRepresentation( cache );
@@ -105,13 +101,6 @@ export const StoreFilesMixin = dedupingMixin( base => {
         .catch( err => {
           console.error( "", err );
         })
-      // TODO: send if-match-since request
-      // TODO:   check response.status
-      // TODO:     handle 200 status code
-      // TODO:       putCache
-      // TODO:       _getFileRepresentation
-      // TODO:     handle 300 status code
-      // TODO:       _getFileRepresentation
     }
 
     _requestFile( fileUrl ) {
@@ -123,7 +112,7 @@ export const StoreFilesMixin = dedupingMixin( base => {
           return this._getFileRepresentation( resp );
         })
         .then( objectURL => {
-          super.putCache( respToCache );
+          super.putCache( respToCache, fileUrl );
           return objectURL;
         })
         .catch(() => {
@@ -137,20 +126,13 @@ export const StoreFilesMixin = dedupingMixin( base => {
       })
     }
 
-    _getCacheCustom() {
-      if ( this._caches ) {
-        let _cache;
-
-        return this._getCache().then( cache => {
-          _cache = cache;
-          return cache.match( this.getCacheRequestKey( url ));
-        })
-      } else {
-        return Promise.reject();
-      }
+    _getCacheCustom( url ) {
+      return super._getCache().then( cache => {
+        return cache.match( this.getCacheRequestKey( url ));
+      }).then( response => {
+        return Promise.resolve( response );
+      })
     }
-
-  }
 
   }
 
