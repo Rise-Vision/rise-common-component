4ad7bc29f5c4578d91bae8be6c80e9560a4786dc
Stuart Lees <stulees@users.noreply.github.com>
2020-06-29 08:42:15 -0400

    Add class to StoreFilesMixin for storing timestamp of last HEAD request in LocalStorage (#54)

--- src/store-files-mixin.js
+++ src/store-files-mixin.js
@@ -7,6 +7,7 @@ export const StoreFilesMixin = dedupingMixin( base => {
       refresh: 1000 * 60 * 60 * 2,
       expiry: 1000 * 60 * 60 * 4
     },
+    LOCAL_STORAGE_KEY = "rise_files_last_requested",
     deletedFileStatusCodes = [ 401, 403, 404 ],
     fileStatuses = {
       fresh: "fresh",
@@ -14,10 +15,76 @@ export const StoreFilesMixin = dedupingMixin( base => {
       deleted: "deleted"
     };
 
+  class LastRequestedStorage {
+    constructor() {
+      this._isSupported = this._isLocalStorageSupported();
+    }
+
+    save( fileUrl, timestamp ) {
+      if ( !this._isSupported ) {
+        return;
+      }
+
+      if ( !fileUrl || !timestamp ) {
+        return;
+      }
+
+      const map = this._getMap();
+
+      map.set( fileUrl, timestamp );
+
+      this._saveMap( map );
+    }
+
+    getTimestamp( fileUrl ) {
+      if ( !this._isSupported || !fileUrl ) {
+        return;
+      }
+
+      const map = this._getMap();
+
+      return map.get( fileUrl )
+    }
+
+    _isLocalStorageSupported() {
+      const test = "test";
+
+      try {
+        localStorage.setItem( test, test );
+        localStorage.removeItem( test );
+        return true;
+      } catch ( e ) {
+        return false;
+      }
+    }
+
+    _getMap() {
+      let map;
+
+      try {
+        map = new Map( JSON.parse( localStorage.getItem( LOCAL_STORAGE_KEY )));
+      } catch ( e ) {
+        console.warn( e ); // eslint-disable-line no-console
+        map = new Map();
+      }
+      return map;
+    }
+
+    _saveMap( map ) {
+      if ( !map || !( map instanceof Map )) {
+        return;
+      }
+
+      localStorage.setItem( LOCAL_STORAGE_KEY, JSON.stringify( Array.from( map )));
+    }
+  }
+
+
   class StoreFiles extends CacheMixin( base ) {
     constructor() {
       super();
 
+      this.lastRequestedStorage = new LastRequestedStorage();
       this.cacheConfig = Object.assign({}, CACHE_CONFIG );
     }
 
