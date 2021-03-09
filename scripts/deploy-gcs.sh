54c42faaa66c52d5abf4b612c6344ed9f6e481aa
Ezequiel Conte <ezequielc@gmail.com>
2021-03-09 16:38:39 -0300

    deploy new file to gcs

--- scripts/deploy-gcs.sh
+++ scripts/deploy-gcs.sh
@@ -1,6 +1,6 @@
 COMPONENT_NAME=$1
 TARGET=$2
 
-gsutil cp dist/$COMPONENT_NAME.js $TARGET
+gsutil cp dist/*.js $TARGET
 gsutil -m setmeta -r -h "Cache-Control:private, max-age=600" $TARGET
 gsutil acl -r ch -u AllUsers:R $TARGET
